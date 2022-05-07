045d285109812277cc4c3ec728aaf8620a19d887
Prince Ahmed <codingwithprince@gmail.com>
2022-05-07 12:15:28 +0600

    complete

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,46 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { useState } from 'react';
+
+
+
 
 export default function Home() {
+  const [user, setUser] = useState({
+    isLoggedIn: false,
+    name:'',
+    email:''
+  })
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyBErzJnwItXUZoXX_7HE9vI4RzjOZO0lFI",
+    authDomain: "fir-auth-d106c.firebaseapp.com",
+    projectId: "fir-auth-d106c",
+    storageBucket: "fir-auth-d106c.appspot.com",
+    messagingSenderId: "451585652973",
+    appId: "1:451585652973:web:ea29440e5804ec0fb5f4e4"
+  };
+  
+  const app = initializeApp(firebaseConfig);
+  const provider = new GoogleAuthProvider();
+
+  const handleSignIn = () =>{
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+    .then((res) => {
+    const {displayName, email} = res.user;
+    const currentUser = {
+      name: displayName,
+      email: email,
+      isLoggedIn: true
+    }
+    setUser(currentUser);
+  })
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,58 +50,30 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        {user.isLoggedIn ? <div>
+            <h2>Welcome Mr. {user.name}</h2>
+            <h4>your email is {user.email}</h4>
+          </div> :
+          <div>
+            <h1 className={styles.title}>
+              Welcome to <a href="https://nextjs.org">Next.js!</a>
+            </h1>
+            <button onClick={handleSignIn}>Sign In</button>
+          </div>
+
+        }
+        
+        {/* <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <button onClick={handleSignIn}>Sign In</button>
+        {
+          user && <div>
+            <h2>Welcome Mr. {user.name}</h2>
+            <h4>your email is {user.email}</h4>
+          </div>
+        } */}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
